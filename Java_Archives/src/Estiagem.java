4272b15097c5430dee8caccfd6ebb2f67e314c34
Rakoski <mastrakoski@gmail.com>
2024-02-22 06:58:44 -0300

    Estiagem.java

--- Java_Archives/src/Estiagem.java
+++ Java_Archives/src/Estiagem.java
@@ -17,33 +17,57 @@ public class Estiagem {
         int x;
         int y;
         int cidade = 0;
+        double mediaCompleta = 0;
+
         List<Integer> consumoDeCadaUm = new ArrayList<Integer>();
         List<String> listaDeCidades = new ArrayList<String>();
-        List<Double> listaDeConsumos = new ArrayList<Double>();
+        List<Integer> listaDeConsumos = new ArrayList<Integer>();
         List<String> listaDeMedias = new ArrayList<String>();
+        List<Double> listaDeMediasDaListaDeMedias = new ArrayList<Double>();
 
         System.out.println("n: ");
         n = scanner.nextInt();
 
         while (n != 0) {
+            consumoDeCadaUm.add(cidade++);
+            listaDeCidades.add("Cidade# "+ (consumoDeCadaUm.get(cidade - 1) + 1) + ":");
             for (int i = 0; i < n; i++) {
-                double mediaDeConsumo;
-                consumoDeCadaUm.add(cidade++);
+                int mediaDeConsumo;
 
                 System.out.println("X: ");
                 x = scanner.nextInt();
                 System.out.println("Y: ");
                 y = scanner.nextInt();
 
-                if (x > y) {
-                    throw new IOException("Y maior que X");
-                }
-
-                listaDeConsumos.add(mediaDeConsumo = Math.floor((double) y /x));
-                System.out.println("Cidade# "+ (consumoDeCadaUm.get(cidade - 1) + 1) + ":");
-                listaDeMedias.add(x + " - " + mediaDeConsumo);
+                listaDeConsumos.add(mediaDeConsumo = (int) Math.floor(y /x));
+                listaDeMediasDaListaDeMedias.add((double) mediaDeConsumo);
+                listaDeMedias.add(x + "-" + mediaDeConsumo);
             }
-            System.out.println(listaDeMedias);
+
+            System.out.println(listaDeCidades.get(cidade - 1));
+
+            StringBuilder sb = new StringBuilder();
+            for (String media : listaDeMedias) {
+                sb.append(media).append(" ");
+            }
+            if (sb.length() > 0) {
+                sb.setLength(sb.length() - 1);
+            }
+            System.out.println(sb.toString());
+
+            int totalPares = 0;
+            for (String media : listaDeMedias) {
+                String[] partes = media.split("-");
+                int valor1 = Integer.parseInt(partes[0]);
+                int valor2 = Integer.parseInt(partes[1]);
+                mediaCompleta += (double)(valor1 + valor2) / 2;
+                totalPares++;
+            }
+            mediaCompleta /= totalPares;
+            System.out.println("Consumo médio: " + mediaCompleta);
+
+            listaDeMediasDaListaDeMedias.clear();
+            listaDeMedias.clear();
             System.out.println("n: ");
             n = scanner.nextInt();
         }
