b830303fa0c5120a19d7cd2d60ac298eb74de876
Rakoski <mastrakoski@gmail.com>
2024-03-01 19:05:13 -0300

    Galopeira.java

---
+++ Java_Archives/src/Galopeira.java
@@ -0,0 +1,30 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Galopeira {
+
+
+    public static int contaAsLetras(String palavra) {
+        char[] arrayDeString = palavra.toCharArray();
+        int numero = 0;
+
+        for (int j = 0; j < arrayDeString.length; j++) {
+            numero++;
+        }
+        return numero;
+    }
+
+    public static int main(String[] args) throws IOException {
+        Scanner scanner = new Scanner(System.in);
+
+        int numeroDePalavras = scanner.nextInt();
+
+        for (int i = 0; i < numeroDePalavras; i++) {
+            String palavra = scanner.nextLine();
+
+            int numeroDeLetras = contaAsLetras(palavra);
+            return numeroDeLetras;
+        }
+        return numeroDePalavras;
+    }
+}
